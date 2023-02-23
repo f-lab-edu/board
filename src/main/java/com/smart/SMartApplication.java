3d7fe2bdd4fc4f4a4c4f85471aacf084f3508f10
hoa0217 <gjwjdghk123@naver.com>
2023-02-24 00:22:06 +0900

    init s-mart

---
+++ src/main/java/com/smart/SMartApplication.java
@@ -0,0 +1,13 @@
+package com.smart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SMartApplication.class, args);
+	}
+
+}
