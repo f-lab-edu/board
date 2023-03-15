bf450b3ec98adb2ec345918c8fe06e88d45ab998
hoa0217 <gjwjdghk123@naver.com>
2023-03-16 01:55:49 +0900

    refactor: "HikariCP 명시"
    - SpringBoot2.0부터 HikariCP가 기본 Datasource이며 Java Config 설정 시 jdbc-url을 사용해야하지만 추후 자동설정으로 변경시엔 url을 사용해야한다. 이에 대한 혼돈을 방지하기 위해 따로 명시함.
    
    Related: #2

--- src/main/java/com/smart/config/DataSourceConfig.java
+++ src/main/java/com/smart/config/DataSourceConfig.java
@@ -1,19 +1,19 @@
 package com.smart.config;
 
+import com.zaxxer.hikari.HikariDataSource;
 import javax.sql.DataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class DataSourceConfig {
+
   @Bean
-  @Primary
-  @ConfigurationProperties(prefix="spring.datasource")
+  @ConfigurationProperties(prefix = "spring.datasource.hikari")
   public DataSource dataSource() {
-    return DataSourceBuilder.create().build();
+    return DataSourceBuilder.create().type(HikariDataSource.class).build();
   }
 }
 
