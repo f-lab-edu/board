d444a8b42d187ab1bb1a044ac9c8f524b9b1013c
haileykim2014 <haileykim2014@gmail.com>
2023-03-26 11:19:33 +0900

    feat: UserSecurityService 리팩토링 - UserSecurityService의 loadUserByUsername 리턴값 정정 - Login 페이지 로그인실패시  에러 메세지 노출 related: #3

--- src/main/java/com/smart/user/service/UserSecurityService.java
+++ src/main/java/com/smart/user/service/UserSecurityService.java
@@ -1,27 +1,33 @@
 package com.smart.user.service;
 
+import com.smart.global.error.NotFoundUserException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
+@Service
 @RequiredArgsConstructor
-@Component
 public class UserSecurityService implements UserDetailsService {
   private final UserService userService;
   private final PasswordEncoder passwordEncoder;
 
   @Override
   public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-    com.smart.user.domain.User user = userService.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("nnn"));
+    com.smart.user.domain.User user = userService.findByEmail(email)
+        .orElseThrow(() -> new NotFoundUserException());
 
-    return org.springframework.security.core.userdetails.User.builder()
+    return buildUserDetails(user);
+  }
+
+  private UserDetails buildUserDetails(com.smart.user.domain.User user) {
+    return User.builder()
         .username(user.getName())
         .password(passwordEncoder.encode(user.getPassword()))
         .roles(user.getRole())
         .build();
   }
-
 }
