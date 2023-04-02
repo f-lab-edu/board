d219dcb530f3709c477c9bb9ec27d8d3389b99dc
hoa0217 <gjwjdghk123@naver.com>
2023-04-02 21:32:11 +0900

    refactor: "메일전송실패 테스트 추가"
    - mail 주소가 null일 경우 메일전송에 실패합니다.
    - 올바른 mail주소가 아닐 경우 메일전송에 실패합니다.
    
    Related: #8

--- src/test/java/com/smart/mail/service/MailServiceTest.java
+++ src/test/java/com/smart/mail/service/MailServiceTest.java
@@ -1,9 +1,11 @@
 package com.smart.mail.service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.MailSendException;
 
 @SpringBootTest
 class MailServiceTest {
@@ -14,10 +16,23 @@ class MailServiceTest {
   final String MAIL = "gjwjdghk123@naver.com";
   final String AUTH_CODE = "authCode";
 
-  @DisplayName("실제 메일 발송 여부를 테스트한다.")
+  @DisplayName("메일 발송에 성공하여 실제로 발송됩니다.")
   @Test
-  public void 메일전송() {
+  public void 메일전송성공() {
     mailService.sendAuthMail(MAIL, AUTH_CODE);
   }
 
+  @DisplayName("이메일이 Null일 경우 발송에 실패합니다.")
+  @Test
+  public void 메일전송실패() {
+    Assertions
+        .assertThrows(NullPointerException.class, () -> mailService.sendAuthMail(null, AUTH_CODE));
+  }
+
+  @DisplayName("올바른 이메일이 아닐 경우 발송에 실패합니다.")
+  @Test
+  public void 메일전송실패2() {
+    Assertions
+        .assertThrows(MailSendException.class, () -> mailService.sendAuthMail("?", AUTH_CODE));
+  }
 }
