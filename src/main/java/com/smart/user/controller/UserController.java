0a3f708aa8f54a20047f17c4a8ab212aebab95ec
hoa0217 <gjwjdghk123@naver.com>
2023-04-15 05:06:50 +0900

    style: "마지막 개행 추가"

--- src/main/java/com/smart/user/controller/UserController.java
+++ src/main/java/com/smart/user/controller/UserController.java
@@ -7,8 +7,6 @@ import java.net.URI;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
