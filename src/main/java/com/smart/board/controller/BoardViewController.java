b4725d63e502cf6dfc9a2471a80dab4b9339c3e0
hoa0217 <gjwjdghk123@naver.com>
2023-04-17 06:00:57 +0900

    refactor: "댓글 기능 추가에 따른 게시판 화면 수정"
    - 게시글 상세 화면에 댓글 CRUD 추가
    
    Related: #11

--- src/main/java/com/smart/board/controller/BoardViewController.java
+++ src/main/java/com/smart/board/controller/BoardViewController.java
@@ -1,84 +1,55 @@
 package com.smart.board.controller;
 
-import com.smart.board.controller.dto.BoardDto.BoardInfo;
+import com.smart.board.controller.dto.BoardDto.BoardDetail;
+import com.smart.board.service.BoardService;
+import com.smart.comment.service.CommentService;
 import com.smart.user.domain.CustomUserDetails;
-import java.util.List;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class BoardViewController {
 
-  private final RestTemplate restTemplate = new RestTemplate();
+  private final BoardService boardService;
+  private final CommentService commentService;
 
-  private String prefixUrl = "http://localhost:8080";
+  public BoardViewController(BoardService boardService, CommentService commentService) {
+    this.boardService = boardService;
+    this.commentService = commentService;
+  }
 
   @GetMapping("/boards")
   public String showBoardsPage(Model model,
       @AuthenticationPrincipal CustomUserDetails userDetails) {
-
-    ResponseEntity<List<BoardInfo>> response = restTemplate.exchange(
-        prefixUrl + "/api/v1/boards",
-        HttpMethod.GET,
-        null,
-        new ParameterizedTypeReference<List<BoardInfo>>() {
-        }
-    );
-
-    model.addAttribute("boards", response.getBody());
+    model.addAttribute("boards", boardService.getAllBoard());
     model.addAttribute("userDetails", userDetails);
-
     return "boards";
   }
 
   @GetMapping("/board")
   public String showBoardDetailPage(Long boardId,
       @AuthenticationPrincipal CustomUserDetails userDetails, Model model) {
-
-    ResponseEntity<BoardInfo> response = restTemplate.exchange(
-        prefixUrl + "/api/v1/board/" + boardId,
-        HttpMethod.GET,
-        null,
-        new ParameterizedTypeReference<BoardInfo>() {
-        }
-    );
-
-    model.addAttribute("board", response.getBody());
+    BoardDetail boardDetail = boardService.getBoardDetailByBoardId(boardId);
+    model.addAttribute("board", boardDetail.getBoardInfo());
+    model.addAttribute("comments", boardDetail.getCommentInfos());
     model.addAttribute("userDetails", userDetails);
-
     return "board";
   }
 
   @GetMapping("/board/create")
   public String showBoardCreatePage(@AuthenticationPrincipal CustomUserDetails userDetails,
       Model model) {
-
     model.addAttribute("userDetails", userDetails);
-
     return "boardCreate";
   }
 
   @GetMapping("/board/update")
   public String showBoardUpdatePage(Long boardId,
       @AuthenticationPrincipal CustomUserDetails userDetails, Model model) {
-
-    ResponseEntity<BoardInfo> response = restTemplate.exchange(
-        prefixUrl + "/api/v1/board/" + boardId,
-        HttpMethod.GET,
-        null,
-        new ParameterizedTypeReference<BoardInfo>() {
-        }
-    );
-
-    model.addAttribute("board", response.getBody());
+    model.addAttribute("board", boardService.getBoardByBoardId(boardId));
     model.addAttribute("userDetails", userDetails);
-
     return "boardUpdate";
   }
 }
