303b3f2f1a0ba0f2667c45f3cb3286ecc7621207
hoa0217 <gjwjdghk123@naver.com>
2023-04-30 23:06:56 +0900

    feature: "GlobalException핸들러 추가"
    Related: #17

--- src/main/java/com/smart/global/error/DuplicatedEntityException.java
+++
@@ -1,8 +0,0 @@
-package com.smart.global.error;
-
-public class DuplicatedEntityException extends RuntimeException {
-
-  public DuplicatedEntityException(String entity) {
-    super(entity + "이 중복됩니다.");
-  }
-}
