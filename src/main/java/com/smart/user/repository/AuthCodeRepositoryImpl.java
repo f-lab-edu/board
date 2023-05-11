8026915b0fd33c31d040fe060ac00749d3a94ec7
haileykim2014 <haileykim2014@gmail.com>
2023-05-12 07:29:25 +0900

    refactor: 임시비밀번호 로직변경 related: #21

--- src/main/java/com/smart/user/repository/AuthCodeRepositoryImpl.java
+++ src/main/java/com/smart/user/repository/AuthCodeRepositoryImpl.java
@@ -27,7 +27,20 @@ public class AuthCodeRepositoryImpl implements AuthCodeRepository {
 
   @Override
   public String generateAuthCode() {
-    return UUID.randomUUID().toString();
+    return getTempPassword();
   }
+  public String getTempPassword(){
+    char[] charSet = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+        'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
 
+    String str = "";
+
+    // 문자 배열 길이의 값을 랜덤으로 10개를 뽑아 구문을 작성함
+    int idx = 0;
+    for (int i = 0; i < 10; i++) {
+      idx = (int) (charSet.length * Math.random());
+      str += charSet[idx];
+    }
+    return str;
+  }
 }
